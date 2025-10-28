e54c5a1c5b5dbc233543a32fa479220a703be9e1
DouglasMonteles <silvadouglas328@gmail.com>
2025-10-27 22:23:46 -0300

    added backend structure
    Change-Id: I3a6650f966141e3bd662cc3c85b9f4c80b897fbe

---
+++ backend/src/main/java/br/com/doug/god_level_challenge/GodLevelChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.doug.god_level_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodLevelChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodLevelChallengeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/doug/god_level_challenge/GodLevelChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.doug.god_level_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodLevelChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
