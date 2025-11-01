84ba4125fc5a741a47b5f5d3f95a29ed18e83f56
DouglasMonteles <silvadouglas328@gmail.com>
2025-11-01 18:29:50 -0300

    frontend: structure welcome page
    Change-Id: I8bb524ed4d24ce99ec2a0df24a259e2ad3d4201a

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Welcome } from './pages/welcome/welcome';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: Welcome,
+    pathMatch: "full",
+    title: "welcome",
+  }
+];
